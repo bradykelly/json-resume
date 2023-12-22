141449c0d8a3bfc9cb6e28a978828fe6cdc7e935
= <=>
2023-12-22 14:20:29 +0200

    Cleaned up main

--- Scripts/Skills/main.py
+++ Scripts/Skills/main.py
@@ -1,46 +1,65 @@
-import io
 import os.path
 import sys
 from typing import List
 
+from helpers.skills_serializer import SkillsSerializer
 from models.base_model import JsonResumeBaseModel
 from models.skills import Skills
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# TODO: Add a logging system to log errors and info as well as write to the console.
+
+
+skills_json: str = ""
+
 
 def main(args: List[str]) -> str | JsonResumeBaseModel:
-    # TODD: Accept a command that determines which action to take
-    pass
+    """
+    The main function of the script. In a later version, this function will accept a command
+    that determines which action to take. The commands will be: `serialize` and `deserialize`.
+    If no command is provided, the program will default to `serialize`.
+
+    :param args: A list of command line arguments.
+    :return: A JSON string if the command is `serialize`, or a JsonResumeBaseModel object if the command is `deserialize`.
+    """
+
+    if len(args) < 2:
+        skill_serializer = SkillsSerializer()
+        skills = read_skills_csv(args[1])
+        json = skill_serializer.serialize_skills(skills)
+        return json
 
 
-# TODO: Move to a SkillsEdit class
-def deserialize_skills(skills_list: List[str]) -> Skills:
-    # Convert a list of strings to a Skills object
-    assert isinstance(skills_list, List)
+def read_skills_csv(csv_path: str) -> Skills:
+    """
+    Reads a CSV file from the provided file path and returns a list of strings.
+    Each string in the list represents a skill name read from the file.
 
-
-# TODO: Move to a SkillsEdit class
-def read_skills_json(json_path: object) -> List[str]:
-    # Read a list of strings from the skills.json file
-    if len(json_path) > 0 and os.path.isfile(json_path):
+    :param csv_path: The path to the CSV file.
+    :return: A list of strings representing skill names.
+    :rtype: List[str]
+    :raises FileNotFoundError: If no file is found at the provided path.
+    :raises IOError: If there is an error reading the file.    
+    """
+    if len(csv_path) > 0 and os.path.isfile(csv_path):
         try:
-            with open(json_path, 'r') as file:
-                skills_list: List[str] = file.readlines()
-                return skills_list
+            with open(csv_path, 'r') as file:
+                ret_list: List[str] = file.readlines()
+                return ret_list
         except IOError as e:
             print(f"Error reading skills file: {e}")
     else:
-        raise FileNotFoundError(f"No skills file found at {json_path}")
+        raise FileNotFoundError(f"No skills file found at {csv_path}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ != '__main__':
+    pass
+else:
     if len(sys.argv) < 2:
         # TODO: Check if usage syntax is correct
         print("Usage: python main.py [COMMAND <path>] [path]")
         sys.exit(1)
-    json = read_skills_json(sys.args[1])
+    if len(sys.argv) == 2:
+        main(sys.argv)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
